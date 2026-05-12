2cd4db8ce628e27f014cba9fba619d8f26ffb576
jensenpat <84298137+jensenpat@users.noreply.github.com>
2026-05-11 22:57:32 -0700

    [core] Implement full audio device hotplug support with notification dialog (#2583)
    * Document audio pipeline
    
    * Rename TX DSP docs branding
    
    * Add audio device hotplug dialog
    
    * Tighten audio hotplug dialog copy
    
    * Match audio dialog section label color
    
    ---------
    
    Co-authored-by: Jeremy Fielder <kk7gwy@aethersdr.com>

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -48,6 +48,7 @@
 #include <QSplitter>
 #include <QPointer>
 #include <QLabel>
+#include <QList>
 #include <QMenu>
 #include <QStatusBar>
 #include <QSizeGrip>
@@ -58,6 +59,7 @@
 #include <atomic>
 
 class QAbstractSlider;
+class QMediaDevices;
 
 #include "gui/ClientEqApplet.h"   // ClientEqApplet::Path enum used in
                                    // onEqCutoffsDragRequested signature.
@@ -147,6 +149,15 @@ private:
     void audioStartTx(const QHostAddress& addr, quint16 port);
     void audioStopTx();
     void updatePcAudioTooltip();
+    void setupAudioDeviceChangeMonitor();
+    void scheduleAudioDeviceChangeCheck();
+    void handleAudioDeviceListChanged();
+    void applyAudioDeviceSelection(const QAudioDevice& inputDevice,
+                                   const QAudioDevice& outputDevice,
+                                   bool reinitializePcInput);
+    void resetMissingAudioDevicesToDefault(bool resetInput,
+                                           bool resetOutput,
+                                           bool reinitializePcInput);
     SliceModel* activeSlice() const;
     static const char* tuneIntentName(TuneIntent intent);
     bool panFollowEnabled() const;
@@ -291,6 +302,11 @@ private:
     DxccColorProvider m_dxccProvider;
     AudioEngine*      m_audio{nullptr};
     QThread*          m_audioThread{nullptr};
+    QMediaDevices*    m_audioDeviceMonitor{nullptr};
+    QTimer            m_audioDeviceChangeTimer;
+    QList<QByteArray> m_knownAudioInputIds;
+    QList<QByteArray> m_knownAudioOutputIds;
+    bool              m_audioDeviceDialogOpen{false};
     NetworkDiagnosticsHistory* m_networkDiagnosticsHistory{nullptr};
     QsoRecorder*      m_qsoRecorder{nullptr};
     ClientPuduMonitor* m_finalMonitor{nullptr};
